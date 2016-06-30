6c731d5d3a29b738b4add62df03321526a31228c
juankinder <juangarbiglia@gmail.com>
2016-06-30 14:32:21 -0700

    This approach is working for us

--- ChangelogGenerator.groovy
+++ ChangelogGenerator.groovy
@@ -16,56 +16,78 @@ def getGithubToken() {
 def getGithubApi() {
     def githubApi = System.getenv("GITHUB_API")
     if(!githubApi) {
-	githubApi = "https://api.github.com"
+        githubApi = "https://api.github.com"
     }
     return githubApi
 }
 
 def getResourceFromGithub(String repoApiUrl, String resource) {
     def url = "${repoApiUrl}${resource}"
-    
+
     println "Getting resource from Github: '${url}'"
-    
+
     def slurper = new groovy.json.JsonSlurper()
     def user = getGithubUser()
-    def token = getGithubToken()        
+    def token = getGithubToken()
     def encodedAuth = "${user}:${token}".getBytes().encodeBase64().toString()
-	
+
     URLConnection connection = new URL(url).openConnection()
     connection.setRequestProperty("Authorization", "Basic ${encodedAuth}")
     def response = slurper.parse(new BufferedReader(new InputStreamReader(connection.getInputStream())))
-    
+
     println "Done."
     println ""
-    
+
     return response
 }
 
+commitsResponse = []
+commitPage = 1
+
+def getCommitFromGitHub(String repoApiUrl, String commitSha) {
+
+    def commit
+
+    if (commitsResponse.size() > 0) {
+        commit = commitsResponse.find { it.sha == commitSha }
+    }
+
+    if (commit == null) {
+        if (commitPage == 50) {
+            throw new Exception("Max number of pages reached trying to search a specific commit")
+        }
+        commitsResponse += getResourceFromGithub(repoApiUrl, "/commits?page=" + commitPage + "&per_page=100")
+        commitPage++
+        commit = getCommitFromGitHub(repoApiUrl, commitSha)
+    }
+    return commit
+}
+
 def includePullInLabel(def pullByLabel, def labelTitles, String label, def pr) {
-    String labelTitle   
+    String labelTitle
     if(labelTitles.keySet().contains(label)) {
-	labelTitle = labelTitles[label]
+        labelTitle = labelTitles[label]
     } else {
-	labelTitle = labelTitles["GENERIC_LABEL"]
+        labelTitle = labelTitles["GENERIC_LABEL"]
     }
 
     if(!pullByLabel[labelTitle]) {
         pullByLabel[labelTitle] = []
     }
-    
+
     pullByLabel[labelTitle] += pr
-    
+
     return pullByLabel
 }
 
 def getLabelTitlesMap() {
     return [
-        "analytics": "Analytics changes",
-	"bug": "Bug fixes",
-	"crash": "Crash fixes",
-	"enhancement": "Enhancements",
-	"translation": "Translations",
-	"GENERIC_LABEL": "Merged Pull Requests"
+            "analytics": "Analytics changes",
+            "bug": "Bug fixes",
+            "crash": "Crash fixes",
+            "enhancement": "Enhancements",
+            "translation": "Translations",
+            "GENERIC_LABEL": "Merged Pull Requests"
     ]
 }
 
@@ -76,40 +98,40 @@ def executeGenerator() {
     println "--------------------------------"
     println ""
 
-    
+
 
     if(!getGithubUser() || !getGithubToken()) {
-	println "You need to define the following environment variables before using this script: GITHUB_USERNAME, GITHUB_TOKEN and optionally GITHUB_API."
-	return
+        println "You need to define the following environment variables before using this script: GITHUB_USERNAME, GITHUB_TOKEN and optionally GITHUB_API."
+        return
     }
 
     //Verify Arguments
     if(args.length != 2) {
-	println "Usage: groovy ChangelogGenerator.groovy TAG_START TAG_END"
-	return
+        println "Usage: groovy ChangelogGenerator.groovy TAG_START TAG_END"
+        return
     }
-    
+
     //Arguments
     def tagStart = args[0]
     def tagEnd = args[1]
     def githubApi = getGithubApi()
 
-    
-    //Determine Repo Name 
+
+    //Determine Repo Name
     //using first result of 'git remote' of local git working copy.
     def gitRemoteCmd = "git remote -v"
     def gitRemoteResult = gitRemoteCmd.execute().text
     def gitRemotes = gitRemoteResult.split("\\r?\\n")
-    
+
     if(!gitRemotes) {
-	println "This git working copy has not any remote configured."
-	return
+        println "This git working copy has not any remote configured."
+        return
     }
-    
-    def firstRemote = gitRemotes[0]    
+
+    def firstRemote = gitRemotes[0]
     //Trim remote name and only keep repo name.
-    def githubRepoName = (firstRemote =~ /(.+):(.+).git(.+)/)[0][2]
-    //Generate Repo API Url    
+    def githubRepoName = (firstRemote =~ /(?:http[s]?:\\/\\/)?([^\\/\s]+\\/)(.*)(?:.git)/)[0][2]
+    //Generate Repo API Url
     def repoApiUrl = "${githubApi}/repos/${githubRepoName}"
 
     //Print environment data
@@ -117,7 +139,6 @@ def executeGenerator() {
     println "Start Tag: ${tagStart}"
     println "End Tag: ${tagEnd}"
     println ""
-
 
     //Get Local Git Data
     println "Getting commits from current local Git working copy..."
@@ -127,73 +148,73 @@ def executeGenerator() {
     println "Done."
     println ""
 
-    
     //Get GitHub Data
     def repoInfoResponse = getResourceFromGithub(repoApiUrl, "")
-    def prResponse = getResourceFromGithub(repoApiUrl, "/pulls?state=closed")
+    def prResponse = getResourceFromGithub(repoApiUrl, "/pulls?state=closed&page=1&per_page=100")
     def issuesResponse = getResourceFromGithub(repoApiUrl, "/issues?state=closed")
     def tagsResponse = getResourceFromGithub(repoApiUrl, "/tags")
-    
 
-    def endTagCommitSha = null    
+    def endTagCommitSha = null
     for(def tag : tagsResponse) {
-	if(tag.name.equals(tagEnd)) {
-	    endTagCommitSha = tag.commit.sha
-	    break
-	}
-    }    
-    def tagCommitResponse = getResourceFromGithub(repoApiUrl, "/commits/${endTagCommitSha}")    
+        if(tag.name.equals(tagEnd)) {
+            endTagCommitSha = tag.commit.sha
+            break
+        }
+    }
+    def tagCommitResponse = getResourceFromGithub(repoApiUrl, "/commits/${endTagCommitSha}")
     def tagEndDate = Date.parse("yyyy-MM-dd'T'HH:mm:ss'Z'", tagCommitResponse.commit.author.date)
 
-    
     //Include only pull requests for commits of this version.
-    def versionPullRequests = []       
+    def versionPullRequests = []
+
     for(def commitSha : commits) {
-	for(def pr in prResponse) {
-	    if(commitSha.equals(pr.merge_commit_sha)) {
-	        versionPullRequests += pr
-	    }
-	}
+        def commit = getCommitFromGitHub(repoApiUrl, commitSha)
+
+        for (def parent : commit.parents) {
+            pr = prResponse.find { it.head.sha == parent.sha }
+            if (pr != null) {
+                versionPullRequests += pr
+            }
+        }
     }
 
-    
     //Group pull requests using labels
     def labelTitles = getLabelTitlesMap()
     def pullsByLabel = [:]
     for(def labelTitle : labelTitles.entrySet()) {
-	pullsByLabel[labelTitle.value] = []
+        pullsByLabel[labelTitle.value] = []
     }
-    
+
     for(def issue : issuesResponse) {
         if(issue.pull_request) {
-	    //This issue is a pull request
-	    for(def pr : versionPullRequests) {
-	        if(pr.url.equals(issue.pull_request.url)) {
-		    //This issue is for this pull request
-		    
-		    boolean isIncludedInAtLeastOneLabel = false
-		    
-		    if(issue.labels) {
-		        //Pull Request has labels
-		        for(def label : issue.labels) {
-			    if(label.name in labelTitles.keySet()) {
-			        //PR label is one of the mapped one.
-				isIncludedInAtLeastOneLabel = true
-				includePullInLabel(pullsByLabel, labelTitles, "${label.name}", pr)			      
-			    }
-			}
-		    }
-		    
-		    if(!isIncludedInAtLeastOneLabel) {
-		        //Pull Request does not have any label or those labels are not in the titles map.
-		        includePullInLabel(pullsByLabel, labelTitles, "", pr)
-		    }
-		}
-	    }
-	}
+            //This issue is a pull request
+            for(def pr : versionPullRequests) {
+                if(pr.url.equals(issue.pull_request.url)) {
+                    //This issue is for this pull request
+
+                    boolean isIncludedInAtLeastOneLabel = false
+
+                    if(issue.labels) {
+                        //Pull Request has labels
+                        for(def label : issue.labels) {
+                            if(label.name in labelTitles.keySet()) {
+                                //PR label is one of the mapped one.
+                                isIncludedInAtLeastOneLabel = true
+                                includePullInLabel(pullsByLabel, labelTitles, "${label.name}", pr)
+                            }
+                        }
+                    }
+
+                    if(!isIncludedInAtLeastOneLabel) {
+                        //Pull Request does not have any label or those labels are not in the titles map.
+                        includePullInLabel(pullsByLabel, labelTitles, "", pr)
+                    }
+                }
+            }
+        }
     }
-    
-    
+
+
     println "Processing changelog..."
     StringBuilder sb = new StringBuilder()
 
@@ -207,17 +228,17 @@ def executeGenerator() {
     sb.append("\n")
 
     for(def pulls : pullsByLabel) {
-	if(pulls.value) {
-	    //There is at least one PR in this group
-	    sb.append("\n")
-	    sb.append("**${pulls.key}:**")
-	    sb.append("\n\n")
+        if(pulls.value) {
+            //There is at least one PR in this group
+            sb.append("\n")
+            sb.append("**${pulls.key}:**")
+            sb.append("\n\n")
 
-	    for(def pr : pulls.value) {
-		sb.append("- ${pr.title} [\\#${pr.number}](${pr.html_url}) ([${pr.user.login}](${pr.user.html_url}))")
-		sb.append("\n")
-	    }
-	}
+            for(def pr : pulls.value) {
+                sb.append("- ${pr.title} [\\#${pr.number}](${pr.html_url}) ([${pr.user.login}](${pr.user.html_url}))")
+                sb.append("\n")
+            }
+        }
     }
 
     String changeLog = sb.toString()
@@ -239,10 +260,10 @@ def executeGenerator() {
     def changelogFile = new File('CHANGELOG.md')
     String currentContent = null
     if(changelogFile.exists()) {
-	println "Reading existing CHANGELOG.md file..."
-	currentContent = changelogFile.getText()
-	println "Done."
-	println ""
+        println "Reading existing CHANGELOG.md file..."
+        currentContent = changelogFile.getText()
+        println "Done."
+        println ""
     }
 
 
@@ -250,15 +271,15 @@ def executeGenerator() {
     println "Writing CHANGELOG.md file..."
     changelogFile.write(changeLog)
     if(currentContent) {
-	changelogFile.append("\n")
-	currentContent.eachLine { line, count ->
-	    if (count > 1) {
-		changelogFile.append(line)
-		changelogFile.append("\n")
-	    }
-	}
+        changelogFile.append("\n")
+        currentContent.eachLine { line, count ->
+            if (count > 1) {
+                changelogFile.append(line)
+                changelogFile.append("\n")
+            }
+        }
     }
-    println "Done."    
+    println "Done."
 
 
 
